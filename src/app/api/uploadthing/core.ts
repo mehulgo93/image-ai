270c9767a24dfc9d9117e102e3215f805396c5f3
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2024-10-03 17:30:51 -0400

    setting up image upload tool

---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,25 @@
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
+
+const f = createUploadthing();
+
+const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
+
+export const ourFileRouter = {
+  imageUploader: f({ image: { maxFileSize: "4MB" } })
+    .middleware(async ({ req }) => {
+        // TODO: replace with next-auth
+      const user = await auth(req);
+
+      if (!user) throw new UploadThingError("Unauthorized");
+
+      return { userId: user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+        // store file data with user ID 
+
+      return { url: file.url };
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
