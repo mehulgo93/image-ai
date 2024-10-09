36ccc7ce998f470e2472daab513da649d98a5c87
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2024-10-09 12:48:37 -0400

    initializing database

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,10 @@
 import NextAuth from "next-auth"
 import Github from "next-auth/providers/github";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+
+import { db } from "@/db/drizzle";
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Github],
+  adapter: DrizzleAdapter(db),
 })
