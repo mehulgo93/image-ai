80e35044df486e3cdec0ca26bb34f600b0a8dbb4
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2024-10-14 06:21:55 -0400

    setting up google authentication

--- src/auth.ts
+++ src/auth.ts
@@ -1,15 +1,16 @@
 import NextAuth from "next-auth"
 import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 
 import { db } from "@/db/drizzle";
 
-// creating custom auth pages
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Github],
+  providers: [Github, Google],
   adapter: DrizzleAdapter(db),
   pages: {
     signIn: "/sign-in",
+    error: "/sign-in",
   }
 })
