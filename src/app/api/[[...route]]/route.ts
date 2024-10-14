27390f6faa61bac6c60ee2db102491d2a4d17ce9
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2024-10-14 12:56:27 -0400

    adding oauth configuration

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,11 +1,21 @@
 import ai from "./ai";
-import {Hono} from "hono";
+import test from "./test";
+import {Context, Hono} from "hono";
 import users from "./users";
 import images from "./images";
 import {handle} from "hono/vercel";
+import authConfig from "@/auth.config";
+import { AuthConfig } from "@hono/auth-js";
 
 // Revert to "edge" if planning on running on the edge
 export const runtime = "nodejs";
+
+function getAuthConfig(c: Context): AuthConfig {
+    return {
+        secret: c.env.AUTH_SECRET,
+        ...authConfig
+    }
+}
 
 const app = new Hono().basePath("/api");
 
@@ -13,6 +23,7 @@ const routes = app
 .route("/ai", ai)
 .route("/images", images)
 .route("/users", users)
+.route("/test", test)
 
 export const GET = handle(app);
 export const POST = handle(app);
---
+++ src/app/api/[[...route]]/test.ts
@@ -0,0 +1,10 @@
+import { unsplash } from "@/lib/unsplash";
+import { Hono } from "hono";
+
+
+const app = new Hono()
+.get("/", async (c) => {
+   return c.json({success: true})
+})
+
+export default app;
