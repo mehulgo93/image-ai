9af59fffa00bbd5fe499b591a2d941e9aa8e32ce
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2024-10-17 13:52:48 -0400

    some hooks are not working

--- src/app/api/[[...route]]/projects.ts
+++ src/app/api/[[...route]]/projects.ts
@@ -4,8 +4,22 @@ import { verifyAuth } from "@hono/auth-js";
 import { zValidator } from "@hono/zod-validator";
 import { projects, projectsInsertSchema } from "@/db/schema";
 import { db } from "@/db/drizzle";
+import { eq, and } from "drizzle-orm";
 
 const app = new Hono()
+.get("/:id", verifyAuth(), zValidator("param", z.object({id: z.string()})), async (c) => {
+    const auth = c.get("authUser");
+    const { id } = c.req.valid("param");
+    if (!auth.token?.id) {
+        return c.json({error: "Unauthorized"}, 401);
+    }
+    const data = await db.select().from(projects).where(and(eq(projects.id, id), eq(projects.userId, auth.token.id)));
+    if (data?.length === 0) {
+        return c.json({error: "Project not found"}, 404);
+    }
+    return c.json(data[0]);
+}
+)
 .post("/", verifyAuth(), zValidator("json", 
     projectsInsertSchema.pick({
         name: true,
--- src/app/editor/[projectId]/page.tsx
+++ src/app/editor/[projectId]/page.tsx
@@ -1,8 +1,31 @@
-import { protectServer } from "@/features/auth/utils";
+import { useGetProject } from "@/features/projects/api/use-get-project";
 import { Editor } from "@/features/editor/components/editor";
+import { Loader, TriangleAlert } from "lucide-react";
 
-const EditorProjectIdPage = async () => {
-  await protectServer();
+interface EditorProjectIdPageProps {
+  params: {
+    projectId: string;
+  };
+}
+const EditorProjectIdPage = async ({ params }: EditorProjectIdPageProps) => {
+  const { data, isLoading, isError } = useGetProject(params.projectId);
+
+  if (isLoading || !data) {
+    return (
+      <div className="flex flex-col items-center justify-center h-full">
+        <Loader className="animate-spin size-6 text-muted-foreground" />
+        ...Loading
+      </div>
+    );
+  }
+
+  if (isError) {
+    <div className="flex flex-col items-center justify-center h-full gap-y-5">
+      <TriangleAlert className="size-6 " />
+      <p className="text-muted-foreground text-sm">Failed to load project</p>
+    </div>;
+  }
+
   return <Editor />;
 };
 
